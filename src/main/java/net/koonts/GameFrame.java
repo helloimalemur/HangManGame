bcc9135a946f72fca2920fbd164e09b39c472d01
James Koonts <jkoontsiii@gmail.com>
2022-09-06 10:24:27 -0400

    .

---
+++ src/main/java/net/koonts/GameFrame.java
@@ -0,0 +1,14 @@
+package net.koonts;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameFrame extends JFrame {
+    GameFrame() {}
+
+    @Override
+    public void paintComponents(Graphics g) {
+        super.paintComponents(g);
+    }
+
+}
---
+++ src/main/java/net/koonts/HangMan.java
@@ -0,0 +1,7 @@
+package net.koonts;
+
+public class HangMan {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
